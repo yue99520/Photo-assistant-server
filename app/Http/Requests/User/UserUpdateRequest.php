07cb66c1afc7f7a0fc67d7e5fe90499e9d2a0aa5
ernie <dy99520@gmail.com>
2020-06-23 14:45:01 +0800

    [reconstruct] 正規化Requests

--- app/Http/Requests/User/UserUpdateRequest.php
+++ app/Http/Requests/User/UserUpdateRequest.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Requests\User;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\BaseFormRequest;
 
-class UserUpdateRequest extends FormRequest
+class UserUpdateRequest extends BaseFormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
