356600b0b542a08d47c1fbdd1cf08d77bc312823
ernie <dy99520@gmail.com>
2020-06-23 14:23:53 +0800

    [fix] fix problem "Blueprint morphs() does not accept nullable()" by using "nullableMorphs()"

--- database/migrations/2020_06_04_230220_create_tags_table.php
+++ database/migrations/2020_06_04_230220_create_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class TagMorphsToTaggable extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class TagMorphsToTaggable extends Migration
      */
     public function up()
     {
-        Schema::table('tag', function (Blueprint $table) {
-            $table->morphs("taggable");
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->timestamps();
         });
     }
 
@@ -25,6 +27,6 @@ class TagMorphsToTaggable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('tags');
     }
 }
--- database/migrations/2020_06_21_082109_tags_morph_to_taggable.php
+++ database/migrations/2020_06_21_082109_tags_morph_to_taggable.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagTable extends Migration
+class TagsMorphToTaggable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateTagTable extends Migration
      */
     public function up()
     {
-        Schema::create('tag', function (Blueprint $table) {
-            $table->id();
-            $table->string("title")->nullable(false);
-            $table->timestamps();
+        Schema::table('tags', function (Blueprint $table) {
+            $table->nullableMorphs("taggable");
         });
     }
 
@@ -27,6 +25,6 @@ class CreateTagTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tag');
+        //
     }
 }
